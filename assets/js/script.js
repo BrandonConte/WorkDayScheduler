83e2147ab917a7495421971cc757d3400d426f14
BrandonConte <bconte950@gmail.com>
2021-10-31 14:48:58 -0400

    added in rest of the blocked times if else statments. Trying to get the info to save.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Will display todays date
 var today = moment().format('dddd, MMM Do YYYY');
 
-$("activeDay").html(today);
+$("#currentDay").html(today);
 
 $(document).ready(function() {
     // Makes the save button listener
@@ -17,11 +17,32 @@ $(document).ready(function() {
     })
 
     function timeKeeper() {
-        
+        // Gets number of hours actively
         var timeNow = moment().hour();
+        // loop for the blocked times
         $(".timeBlock").each(function() {
             var blockedTime = parseInt($(this).attr('id').split('hour')[1]);
 
+            // Check times and add the classes.
+            if(blockedTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockedTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass('future');
+            }
         })
     }
+
+    $("#8AM .description").val(localStorage.getItem("8AM"));
+
+    timeKeeper();
 })
